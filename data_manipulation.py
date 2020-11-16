fb9740392ae2d51359b6e68e58b0422ebc9ecb87
Erik Husgafvel <erik.husgafvel@epfl.ch>
2020-11-16 18:19:07 +0200

    Data frame manipulation

--- data_manipulation.py
+++ data_manipulation.py
@@ -7,7 +7,7 @@ Created on Mon Nov 16 17:11:37 2020
 
 import pandas as pd
 
-columns_names = ["co2"]
+columns_names = ["iso_code", "country", "year" ,"co2"]
 
 name_of_country = ["URY", "PER", "CUB", "USA",  "ISL", "FIN", "FRA", "GRC", "MAR", 
                    "ZAF", "CMR", "SAU", "IND", "MNG", "HKG", "JPN", "SGP", "NZL", "FJI"
@@ -20,4 +20,6 @@ data_f = pd.DataFrame(columns = data_.columns)
 
 for i in range(len(name_of_country)):
     data_f = data_f.append(data_.loc[data_['iso_code'] == name_of_country[i]])
-    
+
+data_f_2 = data_f[columns_names]
+data_f_3 = data_f_2.loc[data_f_2['year'] >= 1950]
