04b0f9f6c59a5168e905a7346e667936a35f0ca0
Tomi Räsänen <tb.rasanen@gmail.com>
2020-11-17 10:34:01 +0200

    Correct data file and updated manipulations file

--- data_manipulation.py
+++ data_manipulation.py
@@ -2,10 +2,11 @@
 """
 Created on Mon Nov 16 17:11:37 2020
 
-@author: Tomi
+@author: Tomi Räsänen, Erik Husgafvel
 """
 
 import pandas as pd
+import numpy as np
 
 columns_names = ["iso_code", "country", "year" ,"co2"]
 
@@ -19,7 +20,22 @@ data_f = pd.DataFrame(columns = data_.columns)
 
 
 for i in range(len(name_of_country)):
-    data_f = data_f.append(data_.loc[(data_['iso_code'] == name_of_country[i]) & (data_['year'] >= 1950)])
+    data_f = data_f.append(data_.loc[(data_['iso_code'] == name_of_country[i]) 
+                                     & (data_['year'] >= 1950)])
 
 data_f_2 = data_f[columns_names]
-#data_f_3 = data_f_2.loc[data_f_2['year'] >= 1950]
+
+# To use data the most comfortable way, we will place countries as columns 
+# (groups) and years as rows
+
+grouped_data = pd.DataFrame()
+for i in range(len(name_of_country)):
+    h = data_f_2.loc[data_f_2['iso_code'] == name_of_country[i]]
+    
+    #Different countries have different row indexes in original data, 
+    # which has to be changed to the same.
+    h.index = np.arange(0, len(h))
+    
+    grouped_data.insert(i,name_of_country[i],h['co2'])
+
+grouped_data.to_csv('data_co2.csv', index = False)
